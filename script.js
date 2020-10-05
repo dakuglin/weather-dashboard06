5f9d8e6606c695e17fc5220ec601c212263ce69d
Dana Kuglin <dakuglin@gmail.com>
2020-10-05 10:02:49 -0600

    updated files

--- script.js
+++ script.js
@@ -30,10 +30,15 @@ $(document).ready(function() {   //goes in every jQuery script
 
             var latitude = response.coord.lat; //needed for uv index
             var longitude = response.coord.lon; //needed for uv index
-        
+
+            //var icon = response.weather[0].icon;
+            //console.log(icon);
+
+            var img = $("#cityName").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+            console.log(img)
 
             //display main portion of city weather content
-            var cityName = $("#cityName").text(response.name + " " + "(" + date + ")" ).append(cityName) //city name
+            var cityName = $("#cityName").text(response.name + " " + "(" + date + ")" + img).append(cityName) //city name
             //cityName.addClass("search")
 
             // Convert the temp to fahrenheit
@@ -83,19 +88,40 @@ $(document).ready(function() {   //goes in every jQuery script
                 $("#forecast").text("5-Day-Forecast:");
 
                 // Convert the temp to fahrenheit
-                // var tempIndex = 0;
-                // var arr = ["1","2","3","4","5"]
-                for (var i=0; i < 5; i++) {
-                    var tempK = resp.list[i].main.temp;
-                    console.log(tempK)
-                    var tempF = (tempK - 273.15) * 1.80 + 32;
-                    console.log(tempF)
-                };
+                // for (var i=0; i < 5; i++) {
+                //     var tempK = resp.list[i].main.temp_max;
+                //     var tempF = (tempK - 273.15) * 1.80 + 32;
+                //     console.log(tempF)
+                //     //var forecastTempF = tempF.split()
+                //     var humidity = resp.list[i].main.humidity;
+                //     console.log(humidity)  
+                // };
+             
+                tempF = ["1","2","3","4","5"];
 
-                var day1Temp = $("#forecastTemp1").text(tempF.toFixed(2) + " F").append(day1Temp)
+                var day1Temp = $("#forecastTemp1").text("Temp: " + tempF[0] + " F").append(day1Temp);
+                var day2Temp = $("#forecastTemp2").text("Temp: " + tempF[1] + " F").append(day2Temp);
+                var day3Temp = $("#forecastTemp3").text("Temp: " + tempF[2] + " F").append(day3Temp);
+                var day4Temp = $("#forecastTemp4").text("Temp: " + tempF[3] + " F").append(day4Temp);
+                var day5Temp = $("#forecastTemp5").text("Temp: " + tempF[4] + " F").append(day5Temp);
+
+                humidityForecast = ["1","2","3","4","5"];
+
+                var day1Humidity = $("#forecastHum1").text("Humidity: " + humidityForecast[0] +" %").append(day1Humidity); 
+                var day2Humidity = $("#forecastHum2").text("Humidity: " + humidityForecast[1] +" %").append(day2Humidity); 
+                var day3Humidity = $("#forecastHum3").text("Humidity: " + humidityForecast[2] +" %").append(day3Humidity); 
+                var day4Humidity = $("#forecastHum4").text("Humidity: " + humidityForecast[3] +" %").append(day4Humidity); 
+                var day5Humidity = $("#forecastHum5").text("Humidity: " + humidityForecast[4] +" %").append(day5Humidity); 
+
+                $(".col-md-9").removeClass("display")
+
+                var date1 = $("#date1").text("(10/6/2020)").append(date1);
+                var date2 = $("#date2").text("(10/7/2020)").append(date2);
+                var date3 = $("#date3").text("(10/8/2020)").append(date3);
+                var date4 = $("#date4").text("(10/9/2020)").append(date4);
+                var date5 = $("#date5").text("(10/10/2020)").append(date5);
+
                 
-
-
 
             }); 
 
@@ -107,25 +133,9 @@ $(document).ready(function() {   //goes in every jQuery script
              $("#search1").val(localStorage.getItem('City'));
  
  
-
-    
         });
 
     });
-
-
-
-
-
-
-
-    $("#find-city").on("click", function() {  //listener 
-
-       
-       
-    });
-
-
 
 
 
